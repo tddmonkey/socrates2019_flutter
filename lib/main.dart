767bccab8e5f580bb3d9f74111166ea2b88f1385
Colin Vipurs <zodiaczx6@gmail.com>
2019-06-08 18:58:30 +0100

    Nice pair of pages you have there

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-
 void main() {
 //  debugPaintSizeEnabled = false; // Remove to suppress visual layout
 
@@ -24,16 +23,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo',
-      theme: ThemeData.light(),
-      home: RowAndColumnThing()
-///      ),
-    );
+        title: 'Flutter layout demo',
+        theme: ThemeData.light(),
+        home: RowAndColumnThing()
+
+        ///      ),
+        );
   }
 }
 
 class RowAndColumnThing extends StatelessWidget {
   double screenWidth;
+
+  BuildContext context;
 
   Widget buildColumn() {
     return Column(
@@ -42,26 +44,31 @@ class RowAndColumnThing extends StatelessWidget {
         buildRow("images/chair-alpha.png", "I am chair"),
         buildRow("images/binoculars-alpha.png", "I see things"),
         buildRow("images/flippers-alpha.png", "I swim"),
+        addNavButton()
       ],
     );
   }
 
   var t = TextStyle(
     fontWeight: FontWeight.bold,
-     fontFamily: 'Raleway',
+    fontFamily: 'Raleway',
     fontFamilyFallback: <String>[
-       'Noto Sans CJK SC',
-       'Noto Color Emoji',
-     ],
-   );
+      'Noto Sans CJK SC',
+      'Noto Color Emoji',
+    ],
+  );
 
   Widget buildRow(String imageUrl, String description) {
     return Row(
       children: [
-        Hero(tag: imageUrl, child: SizedBox(width: screenWidth/4, child: Image.asset(imageUrl))),
+        expandableImage(imageUrl),
         Container(
           padding: EdgeInsets.all(15),
-          child: Text(description, style: t, textScaleFactor: 2,),
+          child: Text(
+            description,
+            style: t,
+            textScaleFactor: 2,
+          ),
         )
       ],
     );
@@ -69,17 +76,85 @@ class RowAndColumnThing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    this.screenWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
+    this.context = context;
+    this.screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
         appBar: AppBar(
           title: Text('Flutter layout demo'),
         ),
-        body: buildColumn()
-    );
+        body: buildColumn());
 //    return buildColumn();
+  }
+
+  Widget expandableImage(String imageUrl) {
+//    return Hero(tag: imageUrl, child: SizedBox(width: screenWidth/4, child: Image.asset(imageUrl)));
+    return SizedBox(width: screenWidth / 4, child: Image.asset(imageUrl));
+  }
+
+  Widget addNavButton() {
+    return Center(
+        child: RaisedButton(
+            child: Text("Click Me"), onPressed: navToSomeOtherPage()));
+  }
+
+  navToSomeOtherPage() {
+    return () => {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => GarbageScreen()))
+        };
   }
 }
 
+class GarbageScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Garbage Page'),
+      ),
+      body: FullPageLayoutForItem()
+    );
+  }
+}
+
+class FullPageLayoutForItem extends StatelessWidget {
+  double imageSize;
+
+  @override
+  Widget build(BuildContext context) {
+    this.imageSize = MediaQuery.of(context).size.width * 0.75;
+
+    return Container(
+        padding: const EdgeInsets.all(8.0),
+        alignment: Alignment.topLeft,
+        // Use background color to emphasize that it's a new route.
+//        color: Colors.green,
+        child: Hero(
+            tag: 'flippers',
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  imageBox(),
+                  textBox()
+                ]
+            )
+        )
+    );
+  }
+
+  SizedBox imageBox() {
+    return SizedBox(
+              width: imageSize,
+              child: Image.asset(
+                'images/flippers-alpha.png',
+              )
+          );
+  }
+
+  Widget textBox() {
+    return Text(
+        "These are some flippers.  They're pretty cool if you wanna go in the sea",
+        textScaleFactor: 2
+    );
+  }
+}
